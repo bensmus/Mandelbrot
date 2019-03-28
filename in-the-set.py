0f2492236306f068408188179611efc3e553a3c9
Ben Smus <bensmus@Bens-MacBook-Pro.local>
2019-03-28 04:56:38 -0700

    got iterative mandlebrot working

--- in-the-set.py
+++ in-the-set.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# each complex number is simply a python list with two elements
+
 def zAdd(z1, z2):
     '''vector addition'''
     x1 = z1[0]
@@ -17,15 +19,29 @@ def zSquared(z):
     y = z[1]
     return [x**2 - y**2, 2*x*y]
 
-def mandleFunc(c):
+def mandleFunc(z, c):
     '''z(c) = z^2 + c, where c is the initial
     complex number'''
-    z = zAdd(zSquared(c), c)
+    z = zAdd(zSquared(z), c)
     return z
 
 c = [1, 0]
-z = mandleFunc(c)
+z = mandleFunc(c, c)
 print(z)
+
+def iterMandle(c, itercount):
+    z = c
+    for i in range(itercount):
+        z = mandleFunc(z, c)
+        print(z)
+
+print('unbounded sequence')
+iterMandle(c, 10)
+
+c2 = [-1, 0]
+print('bounded sequence')
+iterMandle(c2, 10)
+
 '''
 plt.plot(x, y)
 plt.show()
